a5744640530a659ce40d3e262db6195e2f29aacd
Daddyelkys <kotanenelkku6@gmail.com>
2025-02-25 12:57:53 +0200

    more changes

--- Assets/Scripts/Enemies/Enemy_Movement.cs
+++ Assets/Scripts/Enemies/Enemy_Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
+    public float gravity = -9.81f * 2;
+    Vector3 velocity;
     public GameObject Player;
     public GameObject Stevfe;
     public float speed;
@@ -11,6 +13,6 @@ public class Enemy_Movement : MonoBehaviour
     void Update()
     {
         Stevfe.transform.position = Vector3.MoveTowards(Stevfe.transform.position, Player.transform.position, speed);
-        
+        velocity.y += gravity * Time.deltaTime;
     }
 }
