98b2104c79c4bf37b6b5f3095a609e6d57304775
Daddyelkys <kotanenelkku6@gmail.com>
2025-02-27 14:54:16 +0200

    shoting

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,42 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player; 
-    public float smoothSpeed = 0.125f; 
-    public Vector3 offset; 
-    public bool lockRotation = true; 
-    public Vector3 fixedRotation = new Vector3(45f, 0f, 0f);
-
+    private Vector3 cameraTarget;
+    private Transform target;
+    // Start is called before the first frame update
     void Start()
     {
-       
-        if (lockRotation)
-        {
-            transform.rotation = Quaternion.Euler(fixedRotation);
-        }
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        if (player == null)
-        {
-            Debug.LogWarning("Hello");
-            return;
-        }
-
-       
-        Vector3 desiredPosition = player.position + offset;
-
-        
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-
-      
-        if (lockRotation)
-        {
-            transform.rotation = Quaternion.Euler(fixedRotation);
-        }
+        cameraTarget = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.position = Vector3.Lerp(transform.position, cameraTarget, Time.deltaTime * 8); 
     }
-
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
@@ -11,14 +13,47 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 8;  
 
     private Quaternion targetRotation;
+
+    public Gun gun;
     private CharacterController Controller;
+    private Camera cam;
 
     void Start()
     {
         Controller = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
     void Update()
+    {
+        ControlMouse();
+        //ControlWASD();
+
+        if(Input.GetButtonDown("Shoot"))
+        {
+            gun.Shoot();
+        }
+        else if (Input.GetButton("Shoot"))
+        {
+            gun.ShootContinuous();
+        }
+    }   
+    void ControlMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.transform.position.y - transform.position.y));
+        targetRotation = Quaternion.LookRotation(mousePos - new Vector3(transform.position.x,0,transform.position.z));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 Motion = input;
+        Motion *= (Mathf.Abs(input.x) == 1 && MathF.Abs(input.z) == 1)?.7f:1;
+        Motion *= (Input.GetButton("Run"))?runSpeed:walkSpeed;
+        Motion += Vector3.up * -8;
+
+        Controller.Move(Motion * Time.deltaTime);
+    }
+    void ControlWASD()
     {
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
