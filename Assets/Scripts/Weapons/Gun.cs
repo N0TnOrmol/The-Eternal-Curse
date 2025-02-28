cf6c51cfd92c29c3aad6da56bac585f953def559
Daddyelkys <kotanenelkku6@gmail.com>
2025-02-28 12:38:06 +0200

    oapsidpoaida

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -1,29 +1,65 @@
 using UnityEngine;
+using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
 public class Gun : MonoBehaviour
 {
     public enum GunType {Semi, Burst, Auto};
     public GunType gunType;
+    public float rpm;
     public Transform spawn;
+    // System:
+    private float SecondsBetweenShots;
+    private float nextPossibleShootTime;
+    public string Target = "Enemy";
+
+    void Start()
+    {
+        SecondsBetweenShots = 10/rpm;
+    }
     public void Shoot()
     {
-        Ray ray = new Ray(spawn.position,spawn.forward);
-        RaycastHit hit;
-
-        float shotDistance = 20;
-
-        if(Physics.Raycast(ray,out hit,shotDistance))
+        if (CanShoot())
         {
-            shotDistance = hit.distance;
+            Ray ray = new Ray(spawn.position,spawn.forward);
+            RaycastHit hit;
+            float shotDistance = 20;
+            if(Physics.Raycast(ray,out hit,shotDistance))
+            {
+                shotDistance = hit.distance;
+                if (hit.collider.CompareTag(Target))
+                {
+                    DmgHp dmgHp = hit.collider.GetComponent<DmgHp>();
+                    if (dmgHp != null)
+                    {
+                        dmgHp.TakeDamage();
+                    }
+                }
+                Debug.DrawRay(ray.origin,ray.direction * shotDistance,Color.red,1);
+                nextPossibleShootTime = Time.time + SecondsBetweenShots;
+                GetComponent<AudioSource>().Play();
+            }
         }
-        Debug.DrawRay(ray.origin,ray.direction * shotDistance,Color.red,1);
     }
-
     public void ShootContinuous()
     {
         if(gunType == GunType.Auto)
         {
             Shoot();
         }
+    } 
+    private bool CanShoot()
+    {
+        bool CanShoot = true;
+        if(Time.time < nextPossibleShootTime)
+        {
+            CanShoot = false;
+        }
+        return CanShoot;
     }
+    private void ShootRayCast()
+    {
+
+    }
+    
 }
