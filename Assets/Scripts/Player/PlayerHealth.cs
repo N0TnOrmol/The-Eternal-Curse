821d154c2e51edbb547dfc6db0e3eede17e74c7d
Daddyelkys <kotanenelkku6@gmail.com>
2025-02-28 12:43:34 +0200

    player health

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+   public int DamageTaken = 2;
+    public int Health;
+    public int MaxHealth = 20;
+    public GameObject Player;
+    private void Start()
+    {
+        Health = MaxHealth;
+    }
+    public void TakeDamage ()
+    {
+        Health -= DamageTaken;
+        if (Health <= 0)
+        {
+            Destroy(Player);
+        }
+    }
+}
