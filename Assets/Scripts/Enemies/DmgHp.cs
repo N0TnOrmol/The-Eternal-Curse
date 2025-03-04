733b5388ba8f330560d261def5ed6df57867a52e
N0TnOrmol <barrieenzo@gmail.com>
2025-03-04 10:22:33 +0200

    update 1
    trying to check if boxtrigger dmg works

--- Assets/Scripts/Enemies/DmgHp.cs
+++ Assets/Scripts/Enemies/DmgHp.cs
@@ -9,11 +9,17 @@ public class DmgHp : MonoBehaviour
     public int Health;
     public int MaxHealth = 20;
     public GameObject Enemies;
+    private PlayerHealth playerHealth;
     private void Start()
     {
+        playerHealth = GetComponent<PlayerHealth>();
         Health = MaxHealth;
     }
+<<<<<<< Updated upstream
     public void TakeDamage ()
+=======
+    public void TakeDamageEnemy ()
+>>>>>>> Stashed changes
     {
         Health -= DamageTaken;
         if (Health <= 0)
@@ -21,4 +27,17 @@ public class DmgHp : MonoBehaviour
             Destroy(Enemies);
         }
     }
+<<<<<<< Updated upstream
 }
+=======
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHealth.TakeDamagePlayer();
+            
+        }
+    }
+}
+>>>>>>> Stashed changes
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int DamageTaken = 2;
+    public int Health;
+    public int MaxHealth = 20;
+    public GameObject Player;
+    public GameObject DmgBox;
+    private void Start()
+    {
+        Health = MaxHealth;
+    }
+    public void TakeDamagePlayer()
+    {
+        Health -= DamageTaken;
+        if (Health <= 0)
+        {
+            Destroy(Player);
+            Debug.Log("You Died");
+        }
+    }
+}
