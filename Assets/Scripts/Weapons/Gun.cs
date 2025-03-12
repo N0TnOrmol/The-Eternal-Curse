b88689fc86f09a703fdc31f7f9fba77c9993ce84
Daddyelkys <kotanenelkku6@gmail.com>
2025-03-12 15:26:23 +0200

    update 8 jose

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -1,21 +1,56 @@
+using System.Collections;
 using UnityEditor.Search;
 using UnityEngine;
 
+  [RequireComponent (typeof(AudioSource))]
 public class Gun : MonoBehaviour
 {
     public enum GunType {Semi,Burst,Auto}
     public GunType gunType;
+    public float rpm;
+
+    // Componenets
     public Transform spawn;
+    public Transform shellEjectionPoint;
+    public LineRenderer tracer;
+    
+    // System
+    float shotDistance = 20;
+    private float secondsBetweenShots;
+    private float nextPossibleShootTime;
+    
+    void Start()
+    {
+        secondsBetweenShots = 60/rpm;
+        if(GetComponent<LineRenderer>())
+        {
+            tracer = GetComponent<LineRenderer>();
+        }
+    }
     public void Shoot()
     {
-        Ray ray = new Ray(spawn.position,spawn.forward);
-        RaycastHit hit;
-        float shotDistance = 20;
-        if(Physics.Raycast(ray, out hit, shotDistance))
+        if(CanShoot())
         {
-            shotDistance = hit.distance;
+            Ray ray = new Ray(spawn.position,spawn.forward);
+            RaycastHit hit;
+
+            
+            if(Physics.Raycast(ray, out hit, shotDistance))
+            {
+                shotDistance = hit.distance;
+            }
+        
+        nextPossibleShootTime = Time.time + secondsBetweenShots;
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+
+        if(tracer)
+        {
+            StartCoroutine("RenderTracer", ray.direction * shotDistance);
         }
-        Debug.DrawRay(ray.origin,ray.direction * shotDistance, Color.red,1);
+        }
+
     }
     public void ShootContinuous()
     {
@@ -23,5 +58,24 @@ public class Gun : MonoBehaviour
         {
             Shoot();
         }
+    }
+    private bool CanShoot()
+    {
+        bool CanShoot = true;
+        
+        if(Time.time < nextPossibleShootTime)
+        {
+            CanShoot = false;
+        }
+        return CanShoot;
+    }
+    IEnumerator RenderTracer(Vector3 hitPoint)
+    {
+        tracer.enabled = true;
+        tracer.SetPosition(0,spawn.position);
+        tracer.SetPosition(1,spawn.position + hitPoint);
+
+        yield return new WaitForSeconds(2);
+        tracer.enabled = false;
     }
 }
