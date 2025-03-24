ee496d31fe3719f4c37c9acf61dedee972fd786e
N0TnOrmol <barrieenzo@gmail.com>
2025-03-24 10:56:43 +0200

    Commit

---
+++ Assets/Scripts/UI/DMController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DMController : MonoBehaviour
+{
+
+    public int DLIndex = 0;
+    public KeyCode TestDM;
+    public GameObject DL1;
+    public GameObject DL2;
+    public GameObject DL3;
+
+    void Update()
+    {
+        DLIndex = Mathf.Clamp(DLIndex, 0, 4);
+        if (Input.GetKeyDown(TestDM))
+        {
+            DLIndex++;
+            if (DLIndex >= 1)
+            {
+                DL1.SetActive(true);
+                if (DLIndex >= 2)
+                {
+                    DL2.SetActive(true);
+                    if (DLIndex >= 3)
+                    {
+                        DL3.SetActive(true);
+                        if (DLIndex >= 4)
+                        {
+                            StartCoroutine(SoberUp());
+                            if (DLIndex <= 0)
+                            {
+                                StopCoroutine(SoberUp());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator SoberUp()
+    {
+        DLIndex--;
+        yield return new WaitForSeconds(10f);
+        DLIndex--;
+        DL3.SetActive(false);
+        yield return new WaitForSeconds(10f);
+        DLIndex--;
+        DL2.SetActive(false);
+        yield return new WaitForSeconds(10f);
+        DLIndex--;
+        DL1.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public KeyCode Pause;
+    public GameObject PauseUI;
+    public bool isPaused = false;
+    public GameObject bullet;
+    private Gun gunScript;
+    private Bullet bulletScript;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Pause) && !isPaused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+            isPaused = true;
+            gunScript.enabled = false;
+            bulletScript.enabled = false;
+            bullet.SetActive(false);
+        }
+        else if (Input.GetKeyDown(Pause) && isPaused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+            isPaused = false;
+            gunScript.enabled = true;
+            bulletScript.enabled = true;
+            bullet.SetActive(true);
+        }
+    }
+
+}
