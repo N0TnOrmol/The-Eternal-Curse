48ea9c1d6af3ed4ac1e6a4632a70e5f948c5ca24
N0TnOrmol <barrieenzo@gmail.com>
2025-04-11 14:23:50 +0300

    level design

--- Assets/Scripts/Enemies/DmgHp.cs
+++ Assets/Scripts/Enemies/DmgHp.cs
@@ -1,49 +1,107 @@
 using System.Collections;
+
+using UnityEditor;
+
 using UnityEditor.Rendering;
+
 using UnityEngine;
+
 public class DmgHp : MonoBehaviour
+
 {
+
     public int DamageTaken = 2;
+
     public int DamageDealt = 2;
+
     public int Health;
+
     public int MaxHealth = 2;
+
     private bool attack = true;
+
     public GameObject Enemies;
+
+    public WaveSystem waveSpawner;
+
+    public string SpawnOrigin;
+
     private void Start()
+
     {
+
         Health = MaxHealth;
+
     }
+
     IEnumerator ContinuousDMG()
+
     {
+
         attack = false;
+
         yield return new WaitForSeconds(5);
+
         attack = true;
+
     }
+
     public void TakeDamageEnemy()
+
     {
+
         if (Random.Range(1, 10) > 9)
+
         {
+
             Health -= DamageTaken * 2; // Multiply DamageTaken by 2 for a critical hit
+
             if (Health <= 0)
+
             {
-                Destroy(Enemies);
+
+                Destroy(gameObject);
+
             }
+
         }
+
         else
+
         {
+
             Health -= DamageTaken;
+
             if (Health <= 0)
+
             {
-                Destroy(Enemies);
+
+                Destroy(gameObject);
+
+                waveSpawner = GameObject.FindGameObjectWithTag(SpawnOrigin).GetComponent<WaveSystem>();
+
+                waveSpawner.waves[waveSpawner.currentWaveIndex].enemiesLeft--;
+
             }
+
         }
+
     }
+
     public void OnTriggerStay(Collider other)
+
     {
+
         if (other.CompareTag("Player") && attack == true)
+
         {
+
             other.GetComponent<PlayerHealth>().TakeDamagePlayer();
+
             StartCoroutine(ContinuousDMG());
+
         }
+
     }
+
 }
--- Assets/Scripts/Enemies/WaveSystem.cs
+++ Assets/Scripts/Enemies/WaveSystem.cs
@@ -1,62 +1,177 @@
 using UnityEngine;
+
 using System.Collections;
 
+
+
 public class WaveSystem : MonoBehaviour
+
 {
+
     [SerializeField] private float countdown;
+
     [SerializeField] private GameObject SpawnPoint;
+
     public Wave[] waves;
+
     public int currentWaveIndex = 0;
+
+    public bool EnemiesAlive = false;
+
     private bool readyToCountDown = true;
+
+
+
     private void Start()
+
     {
+
         readyToCountDown = true;
-        for(int i = 0; i < waves.Length; i++)
+
+        for (int i = 0; i < waves.Length; i++)
+
         {
+
             waves[i].enemiesLeft = waves[i].enemies.Length;
+
         }
+
     }
+
+
+
     private void Update()
+
     {
-        if(currentWaveIndex >= waves.Length)
+
+        if (currentWaveIndex >= waves.Length)
+
         {
-            Debug.Log("GG");
+
             return;
+
         }
-        if(readyToCountDown == true)
+
+        if (readyToCountDown == true)
+
         {
+
             countdown -= Time.deltaTime;
+
         }
-        if (countdown <= 0)
+
+        if (countdown <= 0 && EnemiesAlive == false)
+
         {
+
             readyToCountDown = false;
+
             countdown = waves[currentWaveIndex].timeToNextWave;
+
+            StartCoroutine(EnemiesAliveDelay());
+
             StartCoroutine(SpawnWave());
+
         }
-        if(waves[currentWaveIndex].enemiesLeft == 0)
+
+        CheckForEnemy();
+
+        if (waves[currentWaveIndex].enemiesLeft == 0 && EnemiesAlive == false)
+
         {
+
             readyToCountDown = true;
+
             currentWaveIndex++;
+
         }
+
     }
+
+
+
     private IEnumerator SpawnWave()
+
     {
-        if(currentWaveIndex < waves.Length)
+
+        if (currentWaveIndex < waves.Length)
+
         {
+
             for (int i = 0; i < waves[currentWaveIndex].enemies.Length; i++)
+
             {
+
                 Enemy enemy = Instantiate(waves[currentWaveIndex].enemies[i], SpawnPoint.transform);
+
+                enemy.GetComponent<DmgHp>().SpawnOrigin = gameObject.tag;
+
                 enemy.transform.SetParent(SpawnPoint.transform);
+
                 yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+
             }
-        } 
+
+        }
+
     }
+
+
+
+    private IEnumerator EnemiesAliveDelay()
+
+    {
+
+        yield return new WaitForSeconds(1f);
+
+        EnemiesAlive = true;
+
+    }
+
+
+
+    public void CheckForEnemy()
+
+    {
+
+        GameObject[] RemainingEnemies;
+
+        RemainingEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (RemainingEnemies.Length == 0)
+
+        {
+
+            EnemiesAlive = false;
+
+        }
+
+        else if (RemainingEnemies.Length >= 0)
+
+        {
+
+            EnemiesAlive = true;
+
+        }
+
+    }
+
 }
+
+
+
 [System.Serializable]
-public class Wave   
+
+public class Wave
+
 {
+
     public Enemy[] enemies;
+
     public float timeToNextEnemy;
+
     public float timeToNextWave;
-    [HideInInspector] public int enemiesLeft;
+
+    public int enemiesLeft;
+
 }
