6ffac3d6a3909240a451cce1d1f7fc320fe2fe9d
josepeck <jdlopezcatano@gmail.com>
2025-04-14 11:16:32 +0300

    pause mistake

--- Assets/Scripts/UI/SceneDeterminer.cs
+++ Assets/Scripts/UI/SceneDeterminer.cs
@@ -3,9 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneDeterminer : MonoBehaviour
 {
+    public GameObject gun;
+    public Gun gunScript;
+    public PlayerMovement playerMovement;
+    private Animator playerAnimator;
     public void LoadScene(string sceneName)
     {
+
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
+        
     }
+    /*public void EnableGun()
+    {
+        Cursor.visible = true;
+        if (playerAnimator != null)
+        {
+            playerAnimator.updateMode = AnimatorUpdateMode.Normal;
+            playerAnimator.enabled = true;
+        }
+
+        gun.SetActive(true);
+        gunScript.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+        LineRenderer lr = gun.GetComponent<LineRenderer>();
+        if (lr != null) lr.enabled = true;
+        
+    }*/
 }
