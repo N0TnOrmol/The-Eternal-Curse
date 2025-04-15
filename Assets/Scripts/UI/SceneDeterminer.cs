dfeeb2e5cceb0d02631be196b47b0db0da5334ed
josepeck <jdlopezcatano@gmail.com>
2025-04-15 09:10:16 +0300

    UIchanges

--- Assets/Scripts/UI/SceneDeterminer.cs
+++ Assets/Scripts/UI/SceneDeterminer.cs
@@ -5,6 +5,7 @@ public class SceneDeterminer : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        Cursor.visible = enabled;
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
     }
